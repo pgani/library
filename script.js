e2b92ba218081abfb16fe7e492c4f784850e0e9d
Peter Gani <13806892+pgani@users.noreply.github.com>
2024-01-11 19:26:59 -0500

    Add basic html css code

---
+++ script.js
@@ -0,0 +1,18 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.info = function () {
+        return title + " by " + author + ", " + pages + " pages, " + (read ? "read" : "not read yet");
+    };
+}
+
+function addBookToLibrary() {
+    
+}
+
+function displayBooks() {
+
+}
